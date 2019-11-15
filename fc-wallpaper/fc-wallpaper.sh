6ac866759d88839ec80e997314b95a8119244793
takewo <st0098@gmail.com>
2019-11-15 12:56:10 +0900

    Changed to use anyenv. instead of rbenv.

--- fc-wallpaper/fc-wallpaper.sh
+++ fc-wallpaper/fc-wallpaper.sh
@@ -2,4 +2,8 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 LOG_FILE=$SCRIPT_DIR/fc-wallpaper.log
 cd $SCRIPT_DIR
+
+export PATH="$HOME/.anyenv/bin:$PATH:$HOME/bin"
+eval "$(anyenv init -)"
+
 bundle exec ruby fc-wallpaper.rb ${LOG_FILE}
